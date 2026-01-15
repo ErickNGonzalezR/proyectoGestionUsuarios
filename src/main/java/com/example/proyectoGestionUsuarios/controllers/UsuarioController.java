3157d4ee1f47c825c247e105b65890305817ea95
Erick Nicolas Gonzalez <nicolo288@hotmail.com>
2026-01-15 18:31:52 -0500

    creacion y prueba de la clase UsuatioController

---
+++ src/main/java/com/example/proyectoGestionUsuarios/controllers/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.example.proyectoGestionUsuarios.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+    @RequestMapping(value = "prueba")
+    public String prueba(){
+        return "prueba";
+    }
+}
