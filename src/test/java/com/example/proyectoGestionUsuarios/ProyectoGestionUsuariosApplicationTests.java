df1df1c58811f9ae535ce3ca6aac33a29c2521f6
Erick Nicolas Gonzalez <nicolo288@hotmail.com>
2026-01-15 17:38:28 -0500

    hola mundo

---
+++ src/test/java/com/example/proyectoGestionUsuarios/ProyectoGestionUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoGestionUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGestionUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
