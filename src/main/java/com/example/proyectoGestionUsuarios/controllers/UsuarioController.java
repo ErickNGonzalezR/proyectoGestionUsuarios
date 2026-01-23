bfdf4bc4358032220421fbaa57e5b6be4918ea6b
Erick Nicolas Gonzalez <nicolo288@hotmail.com>
2026-01-22 20:23:29 -0500

    Agregue una nueva funcion getUsuarios para probar que se ve la lista

--- src/main/java/com/example/proyectoGestionUsuarios/controllers/UsuarioController.java
+++ src/main/java/com/example/proyectoGestionUsuarios/controllers/UsuarioController.java
@@ -1,15 +1,21 @@
 package com.example.proyectoGestionUsuarios.controllers;
 
 import com.example.proyectoGestionUsuarios.models.Usuario;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RestController
 public class UsuarioController {
     @RequestMapping(value = "usuario/{id}")
     public Usuario getUsuario(@PathVariable long id){
         Usuario usuario = new Usuario();
+        usuario.setId(id);
         usuario.setNombre("Pepito");
         usuario.setApellido("Perez");
         usuario.setTelefono("4564523");
@@ -17,7 +23,40 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuariosPrueba")
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(123L);
+        usuario.setNombre("Pepito");
+        usuario.setApellido("Perez");
+        usuario.setTelefono("4564523");
+        usuario.setEmail("uncorreo@gmail.com");
+
+        Usuario usuario2 = new Usuario();
+        usuario.setId(12313L);
+        usuario2.setNombre("pito");
+        usuario2.setApellido("rez");
+        usuario2.setTelefono("4564523");
+        usuario2.setEmail("uncorreo@gmail.com");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(1321L);
+        usuario3.setNombre("Pepito");
+        usuario3.setApellido("Perez");
+        usuario3.setTelefono("4564523");
+        usuario3.setEmail("uncorreo@gmail.com");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
+    }
+
+
+
+    @RequestMapping(value = "usuariosPreba")
     public Usuario editar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Pepito");
@@ -26,7 +65,7 @@ public class UsuarioController {
         usuario.setEmail("uncorreo@gmail.com");
         return usuario;
     }
-    @RequestMapping(value = "usuariosPrueba")
+    @RequestMapping(value = "usuariosPruea")
     public Usuario eliminar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Pepito");
@@ -35,7 +74,7 @@ public class UsuarioController {
         usuario.setEmail("uncorreo@gmail.com");
         return usuario;
     }
-    @RequestMapping(value = "usuariosPrueba")
+    @RequestMapping(value = "usuariosPeba")
     public Usuario buscar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Pepito");
